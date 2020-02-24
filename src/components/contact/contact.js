3c5c359f2a1ea93eab1fdf8c40dce5786b0a5ef2
Shadab Ali <shadabali604@gmail.com>
2020-02-24 15:03:09 +0530

    overflow hidden

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -7,7 +7,7 @@ const contact = function(){
 <div className = "contact-info">
 <p><strong>Mobile: </strong>+91-9411078560</p>
 <p><strong>Email: </strong>Shadabali604@gmail.com</p>
-<p className = "connect">connet me on: <span><a href = "https://www.linkedin.com/in/shadabali604/">linkedin</a>, <a href = "https://github.com/shadab-me">GitHub</a></span> </p>
+<p className = "connect">connet me on: <span><a href = "https://www.linkedin.com/in/shadabali604/" target ="blank">linkedin</a>, <a href = "https://twitter.com/dev_shadab" target = "_blank">Twitter</a></span> </p>
 
 </div>
 </div>
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './nav.css';
-
+ 
 
  
 class Nav extends React.Component{
